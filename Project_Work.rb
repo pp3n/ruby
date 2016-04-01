bee416ed05114925604430194f3bf42b3cc4ae57
pp3n <pp3n@protonmail.ch>
2016-04-01 15:46:55 -0400

    Updated ruby

--- Project_Work.rb
+++
@@ -1,6 +0,0 @@
-Project work
-
-1.	Reads in user information that is provided.
-2.	Reads in permissions from a JSON file.
-3.	Creates a new user with the assigned permissions.
-4.	Saves the user in a new users listing file, if file does not exist yet, it creates one.
